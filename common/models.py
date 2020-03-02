4150b14657a1815e1b2a46d79bb6b44e1f134e04
张华明 <zhanghuaming@ijunhai.com>
2020-03-02 15:38:10 +0800

    确定公共模型

---
+++ common/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """
+    基础抽象模型
+    """
+
+    class Meta:
+        verbose_name = '基础抽象模型'
+        abstract = True
+
+    # 公共字段
+    uuid = models.CharField(max_length=32, primary_key=True, verbose_name='UUID')
+    created_by = models.CharField(max_length=32, verbose_name='创建用户UUID')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    updated_by = models.CharField(max_length=32, null=True, verbose_name='更新用户UUID')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+
+
+class SoftDeleteModel(BaseModel):
+    """
+    软删除抽象模型
+    """
+
+    class Meta:
+        verbose_name = '软删除抽象模型'
+        abstract = True
+
+    # 软删除公共字段
+    deleted_by = models.CharField(max_length=32, null=True, verbose_name='删除用户UUID')
+    deleted_at = models.DateTimeField(auto_now=True, verbose_name='删除时间')
+
+
